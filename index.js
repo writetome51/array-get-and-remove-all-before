76dea1e3e5f628fae9f379cfbab247b14cdc2bec
Steve Thompson <writetome51@gmail.com>
2018-11-06 00:05:41 -0700

    updated dependency and added index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {value: true});
+
+exports.getAndRemoveAllBeforeFirst = require('./lib/getAndRemoveAllBeforeFirst').getAndRemoveAllBeforeFirst;
+exports.getAndRemoveAllBeforeLast = require('./lib/getAndRemoveAllBeforeLast').getAndRemoveAllBeforeLast;
